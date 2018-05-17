697d3aac795208250d5784c98072a9148bb5e191
saideep <saideep.chennupati97@gmail.com>
2018-05-17 19:41:16 +0530

    updated for review

--- src/main/java/org/saideep/autosignin/Autosignin.java
+++ src/main/java/org/saideep/autosignin/Autosignin.java
@@ -1,6 +1,5 @@
-package org.saideep.autosignin;
+package test;
 
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,21 +9,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.net.InetAddress;
+
+import java.net.UnknownHostException;
 
 
 
 public class Autosignin {
+	static int a = 1;
+	// checks whether it is reachable or not
+	public void checkconnection() {
+		int timeout = 2000;
+		InetAddress[] addresses = null ;
+		         try {
+	                 addresses = InetAddress.getAllByName("mail.google.com");
+		               } catch (UnknownHostException e1) {
+                           System.out.printf(" Network issue kindly check");			
+			               System.exit(0);
+	 	                                     }
+	  for (InetAddress address : addresses) {
+		         try {
+				     if (address.isReachable(timeout)) {
+				        System.out.printf("%s is reachable%n", address);
+				            a= 1;
+				         }
+				     else {
+				         System.out.printf("%s could not be contacted%n", address);
+				         a = 0;
+				          }
+			             } catch (IOException e) {
+				                 System.out.printf(" Network issue kindly check");				
+				                  System.exit(0);
+			                                      }
+		                } 
+	                }
 
-	public static void main(String[] args) throws InterruptedException {
-		URL stream = Autosignin.class.getResource("/config.txt");
-		 String FILENAME = stream.getFile();      // path to the config file
+	public static void main(String[] args) throws InterruptedException, IOException  {
+		 String FILENAME = "C:\\\\eclipse-workspace\\\\autosignin\\\\src\\\\test\\\\config.txt";        // path to the config file
 		BufferedReader br = null;
 		FileReader fr = null;
 		String sCurrentLine;
 		String [] arrOfStr ;
 		String em = null;
 		String pass = null;
+	    String ver;
+		String per = new String("Sign in");
+		String currenturl ;
+		String tempurl="dummyurl";
 		try{
 			fr = new FileReader(FILENAME);
 			br =  new BufferedReader(fr);
@@ -44,12 +75,15 @@ public class Autosignin {
 
 		}
 		WebDriver gm = new ChromeDriver();                                                          // initialising web driver
-		gm.get("https://mail.google.com");
 		
-	    String ver;
-		String per = new String("Sign in");
-		String currenturl ;
-		String tempurl="dummyurl";
+		 Autosignin check = new Autosignin();
+		 check.checkconnection();
+		 if(a==1) {
+			 gm.get("https://mail.google.com");
+				String sam = gm.getTitle();
+				System.out.println(sam);
+		  // boolean status = elem.isDisplayed();
+		  // System.out.println(elem);
 			gm.findElement(By.xpath(".//*[@id='identifierId']")).sendKeys(Keys.CONTROL + "a");
 			gm.findElement(By.xpath(".//*[@id='identifierId']")).sendKeys(Keys.DELETE);              //for clearing the keys in the search box if exists
 			gm.findElement(By.xpath(".//*[@id='identifierId']")).sendKeys(em);                       //writing email id on to the box
@@ -63,7 +97,7 @@ public class Autosignin {
 			
         }
       else {
-    	  gm.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(Keys.CONTROL + "a");
+    	    gm.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(Keys.CONTROL + "a");
 			gm.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(Keys.DELETE);          //for clearing the keys in the search box if exists				     
 		    gm.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(pass);                 // sending keys to password bax
 		    gm.findElement(By.xpath(".//*[@id='passwordNext']/content/span")).click();                               // clicking the next button
@@ -71,18 +105,27 @@ public class Autosignin {
 		    tempurl = gm.getCurrentUrl();                                                                            //getting the tempurl because if the password is incorrect it stays in the same url else it goes to next step
 			 
 			
-			if( currenturl.equals(tempurl)) {
-			    System.out.println("enterd wrong password in config file");
-			    
-			    }
+			 if( currenturl.equals(tempurl)) {
+			     System.out.println("enterd wrong password in config file");
+			   			    }
 			else {
+				check.checkconnection();
+				if(a==1) {
 				System.out.println("successfully logged in");
-			}
-    	  
-      }
-
-	   
-	    }
+				           }
+				 else {
+					 System.out.println("network issue");
+				 }
+			 }
+			 }
+		     }
+				
+		 else {
+			 System.out.println("network issue");
+		    }
+			
+      
+         }	    
 	}
 
 
